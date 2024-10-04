48f70c1ba32cc92b44186185f9717b0add73f26d
Frederic Jasmin <76606949+fredericjasmin@users.noreply.github.com>
2024-10-03 20:01:09 -0600

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,6 @@ import expressiveCode from 'astro-expressive-code'
 import { expressiveCodeOptions } from './src/site.config'
 import icon from 'astro-icon'
 
-import vercel from '@astrojs/vercel/serverless'
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.me',
@@ -41,8 +39,5 @@ export default defineConfig({
 		}
 	},
 	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	})
+	output: 'static' // Cambiado a 'static' para un build estático
 })
