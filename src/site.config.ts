5414616bb5ede9baef95765776403af75370da6b
Frederic Jasmin <76606949+fredericjasmin@users.noreply.github.com>
2024-10-03 19:49:20 -0600

    Add files via upload

---
+++ src/site.config.ts
@@ -0,0 +1,48 @@
+import type { SiteConfig } from '@/types'
+import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
+
+export const siteConfig: SiteConfig = {
+	author: 'Frederic Jasmin',
+	title: 'Frederic Jasmin',
+	description: 'The official Astro Resume Theme',
+	lang: 'en-GB',
+	ogLocale: 'en_GB',
+	date: {
+		locale: 'en-GB',
+		options: {
+			day: 'numeric',
+			month: 'short',
+			year: 'numeric'
+		}
+	}
+}
+
+export const menuLinks: Array<{ title: string; path: string }> = [
+	{
+		title: 'Home',
+		path: '/'
+	}
+]
+
+export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
+	themes: ['dracula', 'github-light'],
+	themeCssSelector(theme, { styleVariants }) {
+		if (styleVariants.length >= 2) {
+			const baseTheme = styleVariants[0]?.theme
+			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme
+			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`
+		}
+		return `[data-theme="${theme.name}"]`
+	},
+	useThemedScrollbars: false,
+	styleOverrides: {
+		frames: {
+			frameBoxShadowCssValue: 'none'
+		},
+		codeFontSize: '0.875rem',
+		codeLineHeight: '1.7142857rem',
+		borderRadius: '4px',
+		codePaddingInline: '1rem',
+		codeFontFamily: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;'
+	}
+}
